effcbc32f12459e61bd1733f9fe842b74a91f6a9
Yunus YASAR <yunus.yasar@pm.me>
2022-06-10 05:15:52 +0300

    first commit

--- master/jobs/dsl/bootcamp/folder_bootcamp.groovy
+++ master/jobs/dsl/bootcamp/folder_bootcamp.groovy
@@ -1,23 +1,4 @@
 folder('Bootcamp') {
     displayName('Bootcamp Pipelines')
     description('Add all jobs to here for Bootcamp')
-    listView('NodeJS-Bootcamp-Folder'){
-        description('Bootcamp Pipelines')
-        filterBuildQueue()
-        filterExecutors()
-        recurse()
-        jobs {
-            regex('bootcamp-*')
-        }
-        columns {
-            status()
-            weather()
-            name()
-            lastSuccess()
-            lastFailure()
-            lastDuration()
-            buildButton()
-            lastBuildConsole()
-        }
-    }
 }
