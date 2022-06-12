82b1a51b17ddd96e5a7e1855d3b31f66e78988b7
Yunus YAŞAR <yunusy20@gmail.com>
2022-06-12 11:40:26 +0300

    add new job

---
+++ master/jobs/dsl/bootcamp/nodejs/pipeline_nodejs4.groovy
@@ -0,0 +1,31 @@
+pipelineJob('NodeJSPipeline4') {
+
+
+    definition {
+        cpsScm {
+            lightweight(true)
+            scm {
+                gitSCM {
+                    branches {
+                        branchSpec {
+                            name('*/master')
+                        }
+                    }
+                    userRemoteConfigs {
+                        userRemoteConfig {
+                            url('git@github.com:yikiksistemci/the-example-app.nodejs.git')
+                            credentialsId('github-ssh-private-key')
+                            name('origin')
+                            refspec('')
+                        }
+                    }
+                    doGenerateSubmoduleConfigurations(false)
+                    browser {
+                    }
+                    gitTool('')
+                }
+            }
+            scriptPath('./Jenkinsfile.docker')
+        }
+    }
+}
